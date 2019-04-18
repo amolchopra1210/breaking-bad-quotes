4982064d52ce955ac40b08ea31f141e59b243aa7
Amol Chopra <achopra@wundermancommerce.com>
2019-04-18 10:44:49 +0530

    Updates

---
+++ precache-manifest.90528396c9777b13302b2d6d6b5392d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d9758938133d3df3f3",
+    "url": "/breaking-bad-quotes/static/js/runtime~main.a738f69d.js"
+  },
+  {
+    "revision": "14c51f02a9dd4040cc63",
+    "url": "/breaking-bad-quotes/static/js/main.c3116ee8.chunk.js"
+  },
+  {
+    "revision": "655194bfc978ad93c8de",
+    "url": "/breaking-bad-quotes/static/js/2.ec8b0532.chunk.js"
+  },
+  {
+    "revision": "14c51f02a9dd4040cc63",
+    "url": "/breaking-bad-quotes/static/css/main.f6067192.chunk.css"
+  },
+  {
+    "revision": "90af1e093d71557852ab173fc99f145e",
+    "url": "/breaking-bad-quotes/index.html"
+  }
+];
